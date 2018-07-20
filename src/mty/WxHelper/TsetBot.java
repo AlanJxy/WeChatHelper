6435310f5a5ae869160b42dd3d34dd27411d1b63
AlanJxy <jixinyux@qq.com>
2018-07-20 18:34:18 +0800

    zancun

---
+++ src/mty/WxHelper/TsetBot.java
@@ -0,0 +1,22 @@
+package mty.WxHelper;
+
+import io.github.biezhi.wechat.WeChatBot;
+import io.github.biezhi.wechat.api.annotation.Bind;
+import io.github.biezhi.wechat.api.constant.Config;
+import io.github.biezhi.wechat.api.enums.MsgType;
+import io.github.biezhi.wechat.api.model.WeChatMessage;
+
+public class TsetBot extends WeChatBot {
+    public TsetBot(Config builder) {
+        super(builder);
+    }
+    @Bind(msgType = MsgType.TEXT)
+    public void handleText(WeChatMessage message) {
+            log.info("接收到 [{}] 的消息: {}", message.getName(), message.getText());
+            this.sendMsg(message.getFromUserName(), "自动回复: " + message.getText());
+    }
+
+    public static void main(String[] args) {
+        new TsetBot(Config.me().autoLogin(true).showTerminal(true)).start();
+    }
+}
